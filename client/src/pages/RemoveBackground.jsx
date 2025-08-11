e923901c5fc168b1e2e728c1708b576fd4a42ae0
Syed Ausaf Hasib <syedausaf2003@gmail.com>
2025-08-11 17:00:16 +0530

    Barebones of project, initial commit

---
+++ client/src/pages/RemoveBackground.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RemoveBackground = () => {
+  return (
+    <div>
+      <h1>Remove BG</h1>
+    </div>
+  )
+}
+
+export default RemoveBackground
---
+++ client/src/pages/RemoveObject.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RemoveObject = () => {
+  return (
+    <div>
+      <h1>Remove Obj</h1>
+    </div>
+  )
+}
+
+export default RemoveObject
