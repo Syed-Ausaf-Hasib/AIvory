6dc30af948167fe4a89611c2cb8286075f882f29
Syed Ausaf Hasib <syedausaf2003@gmail.com>
2025-08-12 03:08:43 +0530

    Dashboard page completed

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -9,13 +9,13 @@ const Hero = () => {
         {/* Text section */}
         <div className='text-center mb-6'>
             <h1 className='text-3xl sm:text-5xl md:text-6xl 2xl:text-7xl font-semibold mx-auto leading-[1.2]'>
-                Create amazing content with{' '}
+                Create amazing content <br/> with{' '}
                 <span className="animate-gradient bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 bg-clip-text text-transparent font-bold">
                     AI tools
                 </span>
             </h1>
             <p className='mt-4 max-w-xs sm:max-w-lg 2xl:max-w-xl m-auto max-sm:text-xs text-gray-600'>
-                Transform your content <br/> creation with our suite of premium AI tools. Write articles, generate images and enhance your workflow
+                Transform your content creation with our suite of premium AI tools.<br/> Write articles, generate images and enhance your workflow.
             </p>
         </div>
 
