ffaa141e8c69b6100a1460f124c69c3c2d9cb18d
Syed Ausaf Hasib <syedausaf2003@gmail.com>
2025-08-12 21:19:23 +0530

    Community page completed

--- client/src/pages/ReviewResume.jsx
+++ client/src/pages/ReviewResume.jsx
@@ -47,7 +47,7 @@ const ReviewResume = () => {
         </form>
 
         {/* Second Column */}
-        <div className='w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-96'>
+        <div className='w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-96 max-h-[600px]'>
 
           <div className='flex items-center gap-3'>
             <FileTextIcon className='w-6 text-[#08B6CE]'/>
